c43eaf0bddb09a4c195057345f97df4e2e70f288
shaoming <17762734229@163.com>
2023-02-08 10:30:55 +0800

    add

--- scripts/lua/levelup_add_item.lua
+++ scripts/lua/levelup_add_item.lua
@@ -1,13 +1,10 @@
 function levelUpAddItem(event, player)
     local level = player:GetLevel()
-    if (level == 2) then
-        player:AddItem(41599, 4)
-    end
     if (level == 60) then
       if (player:IsHorde()) then
-          player:AddItem(88888, 1)
+          player:AddItem(25474, 1)
       else
-          player:AddItem(88889, 1)
+          player:AddItem(25470, 1)
       end
   end
 end
